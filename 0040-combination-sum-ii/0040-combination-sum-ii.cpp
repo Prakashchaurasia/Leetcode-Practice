20ddc8bb025b9a88e246ac179e5a67064adc8ce7
Prakash Kumar <prakash953449@gmail.com>
2026-08-01 12:15:38 +0530

    Time: 0 ms (100%), Space: 14.4 MB (19.91%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> v;
+    vector<vector<int>> ans;
+    unordered_set<int> st;
+    void h(vector<int>& c, int t,int idx){
+        if(t==0) {
+            ans.push_back(v);
+            return;
+        }       
+        for(int i=idx;i<c.size();i++){
+            if(i > idx && c[i] == c[i-1]) continue;
+            if(c[i] <= t){
+                v.push_back(c[i]);
+                h(c, t - c[i], i + 1);
+                v.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& c, int t) {
+        sort(c.begin(),c.end());
+        h(c,t,0);
+        return ans;
+    }
+};
